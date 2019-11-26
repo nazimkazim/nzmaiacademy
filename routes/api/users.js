5c9291f5258c35d2e1323c9718975b14bde5e476
nazimkazim <nazik_111@mail.ru>
2019-11-26 13:25:32 +0600

    add jwt strategy and middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
+const config = require("config");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../../models/User");
 
@@ -53,7 +55,23 @@ router.post(
 
       await user.save();
 
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      //res.send("User registered");
 
       console.log(req.body);
     } catch (err) {
